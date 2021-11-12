9faf6e4020a005c5bc811b42b57ddae51bbd6796
Alexander Kobelev <ak.mail.ru@gmail.com>
2021-11-12 14:49:29 +0300

    2021-10-14 E-7321 Виктория Попова

--- main-readFiles.R
+++ main-readFiles.R
@@ -1,8 +1,8 @@
-# max - 390 sec, 220-292
-outFile <- "2021-10-25 17-23-43 6 mm.csv"
-interval <- (0 * 1000 + 1):(390 * 1000)
-aper <- read.csv("2021-10-25 17-23-43 246 aper.csv")[interval,]
-plog <- read.csv("2021-10-25 17-23-43 256 PureLogic.csv")[interval,]
+# max - 514 sec
+outFile <- "2021-10-14 15-09-28 7 mm.csv"
+interval <- (0 * 1000 + 1):(514 * 1000)
+aper <- read.csv("2021-10-14 15-09-28 992 aper.csv")[interval,]
+plog <- read.csv("2021-10-14 15-09-29 011 PureLogic.csv")[interval,]
 df <- merge(aper, plog)
 
 step <- 500
@@ -19,15 +19,15 @@ out <- sapply(1:(length(df$TIME) / step - 1),
                 rS2 <- mean(df$R2[interval])
                 rS2_ <- mean(df$R2[intervalNext])
 
-                aRhoS1 <- mean(df$APPARENT_06_18_RHO[interval])
-                aRhoS1_ <- mean(df$APPARENT_06_18_RHO[intervalNext])
-                aRhoS2 <- mean(df$APPARENT_30_18_RHO[interval])
-                aRhoS2_ <- mean(df$APPARENT_30_18_RHO[intervalNext])
+                aRhoS1 <- mean(df$APPARENT_07_21_RHO[interval])
+                aRhoS1_ <- mean(df$APPARENT_07_21_RHO[intervalNext])
+                aRhoS2 <- mean(df$APPARENT_35_21_RHO[interval])
+                aRhoS2_ <- mean(df$APPARENT_35_21_RHO[intervalNext])
 
                 c(df$TIME[center], df$POSITION[center],
                   rS1, rS1_, rS2, rS2_, dhmm,
-                  mean(c(aRhoS1, aRhoS1_)), (aRhoS1_ - aRhoS1) / (dhmm / 18.0),
-                  mean(c(aRhoS2, aRhoS2_)), (aRhoS2_ - aRhoS2) / (dhmm / 18.0)
+                  mean(c(aRhoS1, aRhoS1_)), (aRhoS1_ - aRhoS1) / (dhmm / 21.0),
+                  mean(c(aRhoS2, aRhoS2_)), (aRhoS2_ - aRhoS2) / (dhmm / 21.0)
                 )
               }
 )
@@ -36,10 +36,10 @@ out <- as.data.frame(t(out))
 colnames(out) <- c('TIME', 'POSITION', 'R1', 'R1`', 'R2', 'R2`', 'dh', 'RHO_S1', 'RHO_S1_DIFF', 'RHO_S2', 'RHO_S2_DIFF')
 
 par(mfrow = c(3, 1), mar = c(2, 5, 2, 1), cex = 1.0, family = 'mono', las = 1, tck = 1)
-plot(df$TIME, df$R1, type = 'l', xlab = 'Time, s', ylab = expression(bold(R ~ " 6 x 18 mm")))
+plot(df$TIME, df$R1, type = 'l', xlab = 'Time, s', ylab = expression(bold(R ~ " 7 x 21 mm")))
 lines(out$TIME, out$R1, type = 'b', lty = 'blank', col = 'red')
 
-plot(df$TIME, df$R2, type = 'l', xlab = 'Time, s', ylab = expression(bold(R ~ " 30 x 18 mm")))
+plot(df$TIME, df$R2, type = 'l', xlab = 'Time, s', ylab = expression(bold(R ~ " 35 x 21 mm")))
 lines(out$TIME, out$R2, type = 'b', lty = 'blank', col = 'orange')
 
 plot(df$TIME, df$POSITION, type = 'l', xlab = 'Time, s', ylab = 'POSITION')
@@ -47,10 +47,10 @@ lines(out$TIME, out$POSITION, type = 'b', lty = 'blank', col = 'blue')
 
 
 par(mfrow = c(3, 1), mar = c(2, 5, 2, 1), cex = 1.0, family = 'mono', las = 1, tck = 1)
-plot(df$TIME, df$APPARENT_06_18_RHO, type = 'l', xlab = 'Time, s', ylab = expression(bold(rho ~ " 6 x 18 mm")))
+plot(df$TIME, df$APPARENT_07_21_RHO, type = 'l', xlab = 'Time, s', ylab = expression(bold(rho ~ " 7 x 21 mm")))
 lines(out$TIME, out$RHO_S1, type = 'b', lty = 'blank', col = 'red')
 
-plot(df$TIME, df$APPARENT_30_18_RHO, type = 'l', xlab = 'Time, s', ylab = expression(bold(rho ~ " 30 x 18 mm")))
+plot(df$TIME, df$APPARENT_35_21_RHO, type = 'l', xlab = 'Time, s', ylab = expression(bold(rho ~ " 35 x 21 mm")))
 lines(out$TIME, out$RHO_S2, type = 'b', lty = 'blank', col = 'orange')
 
 plot(df$TIME, df$POSITION, type = 'l', xlab = 'Time, s', ylab = 'POSITION')
--- main.R
+++ main.R
@@ -3,8 +3,8 @@ if (length(ls()) == 0) {
 }
 
 par(mfrow = c(3, 1), mar = c(2, 5, 2, 1), cex = 1.0, family = 'mono', las = 1, tck = 1)
-plot(out$TIME, out$RHO_S1, lty = 'blank', type = 'b', xlab = 'Time, s', ylab = expression(bold(rho[a] ~ " 6 x 18 mm")), col = 'red')
-plot(out$TIME, out$RHO_S2, lty = 'blank', type = 'b', xlab = 'Time, s', ylab = expression(bold(rho[a] ~ " 30 x 18 mm")), col = 'orange')
+plot(out$TIME, out$RHO_S1, lty = 'blank', type = 'b', xlab = 'Time, s', ylab = expression(bold(rho ~ " 7 x 21 mm")), col = 'red')
+plot(out$TIME, out$RHO_S2, lty = 'blank', type = 'b', xlab = 'Time, s', ylab = expression(bold(rho ~ " 35 x 21 mm")), col = 'orange')
 plot(out$TIME, out$POSITION, lty = 'blank', type = 'b', xlab = 'Time, s', ylab = 'Indentation')
 
 
@@ -13,22 +13,22 @@ window <- 10 / (out$TIME[length(out$TIME)] - out$TIME[1]) # 10 sec smoothing
 par(mfrow = c(5, 1), mar = c(2, 6, 2, 1), cex = 1.0, family = 'mono', las = 1, tck = 1)
 
 smthRHO_S1 <- smth(out$RHO_S1, window = window, method = "gaussian")
-plot(out$TIME, out$RHO_S1, lty = 'solid', type = 'l', xlab = 'Time, s', ylab = expression(bold(rho ~ " 6 x 18 mm")), col = 'red',
+plot(out$TIME, out$RHO_S1, lty = 'solid', type = 'l', xlab = 'Time, s', ylab = expression(bold(rho ~ " 7 x 21 mm")), col = 'red',
      ylim = c(min(na.omit(smthRHO_S1)), max(na.omit(smthRHO_S1))))
 lines(out$TIME, smthRHO_S1, col = "black", lty = "solid", lwd = 2)
 
 smthRHO_S1_DIFF <- smth(out$RHO_S1_DIFF, window = window, method = "gaussian")
-plot(out$TIME, out$RHO_S1_DIFF, lty = 'solid', type = 'l', xlab = 'Time, s', ylab = expression(bold(frac(partialdiff ~ rho, partialdiff ~ phi) ~ " 6 x 18 mm")), col = 'orange',
+plot(out$TIME, out$RHO_S1_DIFF, lty = 'solid', type = 'l', xlab = 'Time, s', ylab = expression(bold(frac(partialdiff ~ rho, partialdiff ~ phi) ~ " 7 x 21 mm")), col = 'orange',
      ylim = c(min(na.omit(smthRHO_S1_DIFF)), max(na.omit(smthRHO_S1_DIFF))))
 lines(out$TIME, smthRHO_S1_DIFF, col = "black", lty = "solid", lwd = 2)
 
 smthRHO_S2 <- smth(out$RHO_S2, window = window, method = "gaussian")
-plot(out$TIME, out$RHO_S2, lty = 'solid', type = 'l', xlab = 'Time, s', ylab = expression(bold(rho ~ " 30 x 18 mm")), col = 'red',
+plot(out$TIME, out$RHO_S2, lty = 'solid', type = 'l', xlab = 'Time, s', ylab = expression(bold(rho ~ " 35 x 21 mm")), col = 'red',
      ylim = c(min(na.omit(smthRHO_S2)), max(na.omit(smthRHO_S2))))
 lines(out$TIME, smthRHO_S2, col = "black", lty = "solid", lwd = 2)
 
 smthRHO_S2_DIFF <- smth(out$RHO_S2_DIFF, window = window, method = "gaussian")
-plot(out$TIME, out$RHO_S2_DIFF, lty = 'solid', type = 'l', xlab = 'Time, s', ylab = expression(bold(frac(partialdiff ~ rho, partialdiff ~ phi) ~ " 30 x 18 mm")), col = 'orange',
+plot(out$TIME, out$RHO_S2_DIFF, lty = 'solid', type = 'l', xlab = 'Time, s', ylab = expression(bold(frac(partialdiff ~ rho, partialdiff ~ phi) ~ " 35 x 21 mm")), col = 'orange',
      ylim = c(min(na.omit(smthRHO_S2_DIFF)), max(na.omit(smthRHO_S2_DIFF))))
 lines(out$TIME, smthRHO_S2_DIFF, col = "black", lty = "solid", lwd = 2)
 
