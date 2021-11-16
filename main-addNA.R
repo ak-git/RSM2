0dbbf3e183a642f7b760f444da42ad02285b0490
Alexander Kobelev <ak.mail.ru@gmail.com>
2021-11-16 20:21:22 +0300

    Визуализация решения обратной задачи

--- main-addNA.R
+++ main-addNA.R
@@ -1,5 +1,5 @@
 library(dplyr)
-fileName <- "2021-10-25 17-23-43 6 mm inverse.csv"
+fileName <- "2021-05-12 19-03-11 7 mm inverse.csv"
 data <- read.csv(fileName)
 
 for (t in seq(from = min(data$TIME), to = max(data$TIME), by = 0.5)) {
