a7d767271908f86bfe1deca44de6fa132c492906
Alexander Kobelev <ak.mail.ru@gmail.com>
2021-12-20 11:09:38 +0300

    Визуализация решения обратной задачи

--- main-inverse.R
+++ main-inverse.R
@@ -1,9 +1,9 @@
-# max - 630 sec
-sTime <- 190
-eTime <- sTime + 45
+# max - 499 sec
+sTime <- 50
+eTime <- sTime + 100
 interval <- (sTime * 1000 + 1):(eTime * 1000)
-aper <- read.csv("2021-10-22 18-57-26 754 aper.csv")[interval,]
-plog <- read.csv("2021-10-22 18-57-26 755 PureLogic.csv")[interval,]
+aper <- read.csv("2021-10-14 15-09-28 992 aper.csv")[interval,]
+plog <- read.csv("2021-10-14 15-09-29 011 PureLogic.csv")[interval,]
 df <- merge(aper, plog)
 
 par(mfrow = c(6, 1), mar = c(2, 5, 2, 1), cex = 1.0, family = 'mono', las = 1, tck = 1)
@@ -11,7 +11,7 @@ plot(df$TIME, df$R1, type = 'l', xlab = 'Time, s', ylab = expression(bold(R ~ " 
 plot(df$TIME, df$R2, type = 'l', xlab = 'Time, s', ylab = expression(bold(R ~ " 30 x 18 mm")), lwd = 2)
 plot(df$TIME, df$POSITION, type = 'l', xlab = 'Time, s', ylab = 'INDENTATION', col = 'darkgreen', lwd = 2)
 
-inv <- read.csv("2021-10-22 18-57-26 6 mm inverse.csv")
+inv <- read.csv("2021-10-14 15-09-28 7 mm inverse.csv")
 inv <- inv[sTime < inv$TIME & inv$TIME < eTime,]
 plot(inv$TIME, inv$rho1, type = 'l', xlab = 'Time, s', ylab = expression(bold(rho[1])), col = 'red', lwd = 2)
 plot(inv$TIME, inv$rho2, type = 'l', xlab = 'Time, s', ylab = expression(bold(rho[2])), col = 'orange', lwd = 2)
