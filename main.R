08de95df3ceec8147d4e60a873069ba1eacbf6cf
Alexander Kobelev <ak.mail.ru@gmail.com>
2022-11-07 17:54:49 +0300

    Add fixOhms

--- main.R
+++ main.R
@@ -1,15 +1,27 @@
 mmBase <- 8
-interval <- (180 * 1000 + 1):(195 * 1000)
+fixOhms <- as.integer(readline("switch? 0 - no, 1 - yes")) > 0
+interval <- (1 * 1000 + 1):(80 * 1000)
 df <- read.csv(list.files(pattern = ".csv$"))[interval,]
+
+
+yLab1 <- list(s = mmBase, L = mmBase * 3)
+yLab2 <- list(s = mmBase * 5, L = mmBase * 3)
+if (fixOhms) {
+  df$R2 <- df$R2 * 2 - df$R1 * 2
+  df$R1 <- df$R1 * 2
+  yLab1 <- list(s = mmBase * 2, L = mmBase * 4)
+  yLab2 <- list(s = mmBase * 6, L = mmBase * 4)
+}
 
 # Графики исходных сигналов
 par(mfrow = c(3, 1), mar = c(2, 5, 2, 1), cex = 1.0, family = 'mono', las = 1, tck = 1)
-plot(df$TIME, df$R1, type = 'l', xlab = 'Time, s', col = 'red', lwd = 2, ylab = substitute(bold(R[s ~ x ~ L ~ mm] ~ ~Omega), list(s = mmBase, L = mmBase * 3)))
-plot(df$TIME, df$R2, type = 'l', xlab = 'Time, s', col = 'orange', lwd = 2, ylab = substitute(bold(R[s ~ x ~ L ~ mm] ~ ~Omega), list(s = mmBase * 5, L = mmBase * 3)))
+
+plot(df$TIME, df$R1, type = 'l', xlab = 'Time, s', col = 'red', lwd = 2, ylab = substitute(bold(R[s ~ x ~ L ~ mm] ~ ~Omega), yLab1))
+plot(df$TIME, df$R2, type = 'l', xlab = 'Time, s', col = 'orange', lwd = 2, ylab = substitute(bold(R[s ~ x ~ L ~ mm] ~ ~Omega), yLab2))
 plot(df$TIME, df$POSITION, type = 'l', xlab = 'Time, s', col = 'blue', lwd = 2, ylab = 'POSITION, mm')
 
-step <- 2000
-step2 <- 1000
+step <- 4000
+step2 <- 2000
 centerShift <- 0
 splinePosition <- sapply(0:(length(df$TIME) / step),
                          function(x) {
@@ -36,15 +48,17 @@ colnames(splineR) <- c('TIME', 'R1', 'R2')
 splineR1Line <- spline(splineR$TIME, splineR$R1, xout = df$TIME)
 splineR2Line <- spline(splineR$TIME, splineR$R2, xout = df$TIME)
 par(mfrow = c(3, 1), mar = c(2, 5, 2, 1), cex = 1.0, family = 'mono', las = 1, tck = 1)
-plot(df$TIME, df$R1, type = 'l', xlab = 'Time, s', col = 'red', lwd = 2, ylab = substitute(bold(R[s ~ x ~ L ~ mm] ~ ~Omega), list(s = mmBase, L = mmBase * 3)))
+plot(df$TIME, df$R1, type = 'l', xlab = 'Time, s', col = 'red', lwd = 2, ylab = substitute(bold(R[s ~ x ~ L ~ mm] ~ ~Omega), yLab1))
 lines(splineR$TIME, splineR$R1, type = 'b', lty = 'blank', col = 'black')
 lines(splineR1Line, col = "black")
 
-plot(df$TIME, df$R2, type = 'l', xlab = 'Time, s', col = 'orange', lwd = 2, ylab = substitute(bold(R[s ~ x ~ L ~ mm] ~ ~Omega), list(s = mmBase * 5, L = mmBase * 3)))
+plot(df$TIME, df$R2, type = 'l', xlab = 'Time, s', col = 'orange', lwd = 2, ylab = substitute(bold(R[s ~ x ~ L ~ mm] ~ ~Omega), yLab2))
 lines(splineR$TIME, splineR$R2, type = 'b', lty = 'blank', col = 'black')
 lines(splineR2Line, col = "black")
 plot(df$TIME, df$POSITION, type = 'l', xlab = 'Time, s', col = 'blue', lwd = 2, ylab = 'POSITION, mm')
 lines(splinePosition$TIME, splinePosition$POSITION, type = 'b', lty = 'blank', col = 'black')
+
+paste("h = ", df$POSITION[1], " mm")
 
 pulseR1 <- df$R1 - splineR1Line$y
 pulseR2 <- df$R2 - splineR2Line$y
@@ -63,9 +77,9 @@ pointsR <- as.data.frame(t(pointsR))
 colnames(pointsR) <- c('TIME', 'POSITION', 'R1', 'R2')
 
 par(mfrow = c(3, 1), mar = c(2, 5, 2, 1), cex = 1.0, family = 'mono', las = 1, tck = 1)
-plot(df$TIME, pulseR1, type = 'l', xlab = 'Time, s', col = 'red', lwd = 2, ylab = substitute(bold(R[s ~ x ~ L ~ mm] ~ ~Omega), list(s = mmBase, L = mmBase * 3)))
+plot(df$TIME, pulseR1, type = 'l', xlab = 'Time, s', col = 'red', lwd = 2, ylab = substitute(bold(R[s ~ x ~ L ~ mm] ~ ~Omega), yLab1))
 lines(pointsR$TIME, pointsR$R1, type = 'b', lty = 'blank', col = 'black')
-plot(df$TIME, pulseR2, type = 'l', xlab = 'Time, s', col = 'orange', lwd = 2, ylab = substitute(bold(R[s ~ x ~ L ~ mm] ~ ~Omega), list(s = mmBase * 5, L = mmBase * 3)))
+plot(df$TIME, pulseR2, type = 'l', xlab = 'Time, s', col = 'orange', lwd = 2, ylab = substitute(bold(R[s ~ x ~ L ~ mm] ~ ~Omega), yLab2))
 lines(pointsR$TIME, pointsR$R2, type = 'b', lty = 'blank', col = 'black')
 plot(df$TIME, df$POSITION, type = 'l', xlab = 'Time, s', col = 'blue', lwd = 2, ylab = 'POSITION, mm')
 lines(pointsR$TIME, pointsR$POSITION, type = 'b', lty = 'blank', col = 'black')
@@ -79,8 +93,12 @@ pointsDiffR <- sapply(1:((length(pointsR$TIME) - 2) / 2),
                       }
 )
 pointsDiffR <- as.data.frame(t(pointsDiffR))
-colnames(pointsDiffR) <- c('TIME', 'POSITION', 'dR1', 'dR2')
+if (fixOhms) {
+  colnames(pointsDiffR) <- c('TIME', 'POSITION', 'dR3', 'dR4')
+} else {
+  colnames(pointsDiffR) <- c('TIME', 'POSITION', 'dR1', 'dR2')
+}
 
-mean(splineR1Line$y)
-mean(splineR2Line$y)
+median(splineR1Line$y)
+median(splineR2Line$y)
 aggregate(pointsDiffR, by = list(pointsDiffR$POSITION), FUN = mean)
--- merger.R
+++ merger.R
@@ -15,31 +15,27 @@ colnames(pureLogic2) <- c('TIME', 'POSITION')
 aperMillis <- 0
 aper2 <- sapply(1:(length(aper$TIME)),
                 function(x) {
-                  c(x + aperMillis, aper$R1[x], aper$MYO1[x], aper$R2[x], aper$MYO2[x], aper$CCR[x])
+                  c(x + aperMillis, aper$R1[x], aper$R2[x], aper$CCR[x])
                 }
 )
 aper2 <- as.data.frame(t(aper2))
-colnames(aper2) <- c('TIME', 'R1', 'MYO1', 'R2', 'MYO2', 'CCR')
+colnames(aper2) <- c('TIME', 'R1', 'R2', 'CCR')
 
 df <- merge(pureLogic2, aper2)
 df2 <- sapply(1:(length(df$TIME)),
               function(x) {
-                c((df$TIME[x] - df$TIME[1]) / 1000, df$POSITION[x], df$R1[x], df$MYO1[x], df$R2[x], df$MYO2[x], df$CCR[x])
+                c((df$TIME[x] - df$TIME[1]) / 1000, df$POSITION[x], df$R1[x], df$R2[x], df$CCR[x])
               }
 )
 df <- as.data.frame(t(df2))
 tail(df)
-# interval <- (150 * 1000 + 1):(170 * 1000)
+# interval <- (0 * 1000 + 1):(7 * 1000)
 # df <- df[interval,]
-colnames(df) <- c('TIME', 'POSITION', 'R1', 'MYO1', 'R2', 'MYO2', 'CCR')
+colnames(df) <- c('TIME', 'POSITION', 'R1', 'R2', 'CCR')
 
 par(mfrow = c(3, 1), mar = c(2, 5, 2, 1), cex = 1.0, family = 'mono', las = 1, tck = 1)
 plot(df$TIME, df$POSITION, type = 'l', xlab = 'Time, s', ylab = 'Position, mm', col = 'red', lwd = 2)
-plot(df$TIME, df$R1, type = 'l', xlab = 'Time, s', ylab = 'R, Ohms', col = 'orange', lwd = 2,
-     ylim = c(min(df$R1, df$R2), min(250, max(df$R1, df$R2))))
-lines(df$TIME, df$R2, type = 'l', xlab = 'Time, s', ylab = 'R2, Ohms', col = 'green', lwd = 2)
-plot(df$TIME, df$MYO1, type = 'l', xlab = 'Time, s', ylab = 'EMG1', col = 'black', lwd = 0.5, ylim = c(-0.1, 0.1))
-# plot(df$TIME, df$MYO2, type = 'l', xlab = 'Time, s', ylab = 'EMG2', col = 'black', lwd = 1, ylim = c(-0.5, 0.5))
-# plot(df$TIME, df$CCR, type = 'l', xlab = 'Time, s', ylab = 'CCR, Ohms', col = 'black', lwd = 2)
+plot(df$TIME, df$R1, type = 'l', xlab = 'Time, s', ylab = 'R1, Ohms', col = 'orange', lwd = 2)
+plot(df$TIME, df$R2, type = 'l', xlab = 'Time, s', ylab = 'R2, Ohms', col = 'green', lwd = 2)
 
 write.csv(df, file = 'out.csv', row.names = FALSE)
