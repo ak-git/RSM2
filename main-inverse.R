28046ff2927b16306ccfbdb4b9ef6b9366747b87
Alexander Kobelev <ak.mail.ru@gmail.com>
2022-12-25 17:17:13 +0300

    Добавление графиков с регуляризатором

--- main-inverse.R
+++ main-inverse.R
@@ -19,8 +19,24 @@ plot(df$TIME, df$POSITION, type = 'l', col = col[3], lwd = 2, xlim = xlim, xlab 
      main = 'Position')
 
 # Графики параметров модели
+fileSuffix <- rev(c(0.2, 1.0, 5.0))
+col2 <- hue_pal()(length(fileSuffix))
 plot(inv$TIME, inv$rho1, type = 'l', col = col[4], lwd = 2, xlab = xlab, ylab = expression(Omega %.% m),
      main = expression(bold(rho[1])))
+for (i in seq_along(fileSuffix)) {
+  inv2 <- read.csv(list.files(pattern = paste0("inverse - ", format(fileSuffix[i], nsmall = 1), ".csv$")))
+  lines(inv2$TIME, inv2$rho1, type = 'l', col = col2[i], lwd = 2, lty = i)
+}
+
 plot(inv$TIME, inv$rho2, type = 'l', col = col[5], lwd = 2, xlab = xlab, ylab = expression(Omega %.% m),
      main = expression(bold(rho[2])))
+for (i in seq_along(fileSuffix)) {
+  inv2 <- read.csv(list.files(pattern = paste0("inverse - ", format(fileSuffix[i], nsmall = 1), ".csv$")))
+  lines(inv2$TIME, inv2$rho2, type = 'l', col = col2[i], lwd = 2, lty = i)
+}
+
 plot(inv$TIME, inv$h, type = 'l', col = col[6], lwd = 2, xlab = xlab, ylab = 'mm', main = 'h')
+for (i in seq_along(fileSuffix)) {
+  inv2 <- read.csv(list.files(pattern = paste0("inverse - ", format(fileSuffix[i], nsmall = 1), ".csv$")))
+  lines(inv2$TIME, inv2$h, type = 'l', col = col2[i], lwd = 2, lty = i)
+}
