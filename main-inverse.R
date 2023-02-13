a6cc6256da630b4aa577ade3c2b0eaf6e1abab31
Alexander Kobelev <ak.mail.ru@gmail.com>
2023-02-13 15:59:00 +0300

    Ограничение расчётов, нулевой регуляризатор со сглаженными данными

--- main-inverse.R
+++ main-inverse.R
@@ -1,7 +1,12 @@
 library('scales')
 mmBase <- 7
 
+filter <- function(inv) {
+  return(inv[inv$rho1 < 10 & inv$rho2 < 10 & inv$h < 10,])
+}
+
 inv <- read.csv(list.files(pattern = "inverse.csv$"))
+inv <- filter(inv)
 xlim <- c(min(na.omit(inv$TIME)), max(na.omit(inv$TIME)))
 
 interval <- (xlim[1] * 1000 + 1):(xlim[2] * 1000)
@@ -19,12 +24,13 @@ plot(df$TIME, df$POSITION, type = 'l', col = col[3], lwd = 2, xlim = xlim, xlab 
      main = 'Position')
 
 # Графики параметров модели
-fileSuffix <- rev(c(0.2, 1.0, 5.0))
+fileSuffix <- rev(c(0.0))
 col2 <- hue_pal()(length(fileSuffix))
 plot(inv$TIME, inv$rho1, type = 'l', col = col[4], lwd = 2, xlab = xlab, ylab = expression(Omega %.% m),
      main = expression(bold(rho[1])))
 for (i in seq_along(fileSuffix)) {
   inv2 <- read.csv(list.files(pattern = paste0("inverse - ", format(fileSuffix[i], nsmall = 1), ".csv$")))
+  inv2 <- filter(inv2)
   lines(inv2$TIME, inv2$rho1, type = 'l', col = col2[i], lwd = 2, lty = i)
 }
 legend("topright", legend = fileSuffix, lty = seq_along(fileSuffix), col = col2, horiz = F, cex = 0.7)
@@ -33,6 +39,7 @@ plot(inv$TIME, inv$rho2, type = 'l', col = col[5], lwd = 2, xlab = xlab, ylab = 
      main = expression(bold(rho[2])))
 for (i in seq_along(fileSuffix)) {
   inv2 <- read.csv(list.files(pattern = paste0("inverse - ", format(fileSuffix[i], nsmall = 1), ".csv$")))
+  inv2 <- filter(inv2)
   lines(inv2$TIME, inv2$rho2, type = 'l', col = col2[i], lwd = 2, lty = i)
 }
 legend("topright", legend = fileSuffix, lty = seq_along(fileSuffix), col = col2, horiz = F, cex = 0.7)
@@ -40,6 +47,7 @@ legend("topright", legend = fileSuffix, lty = seq_along(fileSuffix), col = col2,
 plot(inv$TIME, inv$h, type = 'l', col = col[6], lwd = 2, xlab = xlab, ylab = 'mm', main = 'h')
 for (i in seq_along(fileSuffix)) {
   inv2 <- read.csv(list.files(pattern = paste0("inverse - ", format(fileSuffix[i], nsmall = 1), ".csv$")))
+  inv2 <- filter(inv2)
   lines(inv2$TIME, inv2$h, type = 'l', col = col2[i], lwd = 2, lty = i)
 }
 legend("topright", legend = fileSuffix, lty = seq_along(fileSuffix), col = col2, horiz = F, cex = 0.7)
