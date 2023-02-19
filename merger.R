fd81a47a416a126357ed5333381ca1285188aa5d
Alexander Kobelev <ak.mail.ru@gmail.com>
2023-02-19 19:03:09 +0300

    2023-02-02 E-8178 ak 7 мм 2023-02-02 17-45-08

--- merger.R
+++ merger.R
@@ -39,12 +39,11 @@ par(mfrow = c(3, 1), mar = c(3, 5, 2, 1), cex = 1.0, family = 'mono', las = 1, t
 xlab <- 'Время, с'
 col <- hue_pal()(3)
 lwd <- 3
-plot(df$TIME, df$POSITION, type = 'l', xlab = xlab, ylab = 'mm', col = col[1], lwd = lwd,
-     main = 'Положение электродной системы по вертикали')
-plot(df$TIME, df$R1, type = 'l', xlab = xlab, ylab = expression(Omega), col = col[2], lwd = lwd,
+plot(df$TIME, df$R1, type = 'l', xlab = xlab, ylab = expression(Omega), col = col[1], lwd = lwd,
      main = substitute(bold(R[s ~ x ~ L ~ mm]), list(s = mmBase, L = mmBase * 3)))
-plot(df$TIME, df$R2, type = 'l', xlab = xlab, ylab = expression(Omega), col = col[3], lwd = lwd,
+plot(df$TIME, df$R2, type = 'l', xlab = xlab, ylab = expression(Omega), col = col[2], lwd = lwd,
      main = substitute(bold(R[s ~ x ~ L ~ mm]), list(s = mmBase * 5, L = mmBase * 3)))
-
+plot(df$TIME, df$POSITION, type = 'l', xlab = xlab, ylab = 'mm', col = col[3], lwd = lwd,
+     main = 'Положение электродной системы по вертикали')
 
 write.csv(df, file = 'out.csv', row.names = FALSE)
