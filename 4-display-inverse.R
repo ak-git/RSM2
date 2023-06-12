122f6f5f190e12ce571e52226929c7a306d731b3
Alexander Kobelev <ak.mail.ru@gmail.com>
2023-06-12 21:32:59 +0300

    2023-05-26 E-8424 ak рука угол 19-01-27 Отображение модельных и экспериментальных данных

--- 4-display-inverse.R
+++ 4-display-inverse.R
@@ -19,19 +19,7 @@ xlim <- c(min(na.omit(inv$TIME)), max(na.omit(inv$TIME)))
 interval <- (xlim[1] * 1000 + 1):(xlim[2] * 1000)
 source(file = 'read.R')
 
-# Графики исходных сигналов
-par(mfcol = c(3, 2), mar = c(3, 5, 2, 1), cex = 1.0, family = 'mono', las = 1, tck = 1)
-col <- hue_pal()(3)
-xlab <- 'Время, с'
-lwd <- 3
-plot(df$TIME, df$R1, type = 'l', col = col[1], lwd = lwd, xlim = xlim, xlab = xlab, ylab = expression(Omega),
-     main = substitute(bold(R[s ~ x ~ L ~ mm]), list(s = mmBase, L = mmBase * 3)))
-plot(df$TIME, df$R2, type = 'l', xlab = xlab, col = col[2], lwd = lwd, xlim = xlim, ylab = expression(Omega),
-     main = substitute(bold(R[s ~ x ~ L ~ mm]), list(s = mmBase * 5, L = mmBase * 3)))
-plot(df$TIME, df$ANGLE, type = 'l', col = col[3], lwd = lwd, xlim = xlim, xlab = xlab, ylab = 'градусы',
-     main = 'Угол разгибания кисти')
-
-# Графики параметров модели
+# Данные параметров модели
 lineType <- 'l'
 col2 <- hue_pal()(length(fileSuffix))
 
@@ -39,6 +27,33 @@ inv <- lapply(seq_along(fileSuffix), function (i) {
   filter(read.csv(list.files(pattern = paste0("inverse - ", format(fileSuffix[i], nsmall = 1), ".csv$"))))
 })
 
+# Графики исходных сигналов
+par(mfcol = c(3, 2), mar = c(3, 5, 2, 1), cex = 1.0, family = 'mono', las = 1, tck = 1)
+col <- hue_pal()(3)
+xlab <- 'Время, с'
+lwd <- 3
+plot(df$TIME, df$R1, type = 'l', col = col[1], lwd = lwd, xlim = xlim, xlab = xlab, ylab = expression(Omega),
+     main = substitute(bold(R[s ~ x ~ L ~ mm]), list(s = mmBase, L = mmBase * 3)),
+     ylim = c(min(sapply(inv, function (i) {min(na.omit(i$PREDICTED_R1))}), df$R1), max(sapply(inv, function (i) {max(na.omit(i$PREDICTED_R1))}), df$R1)))
+lapply(seq_along(fileSuffix), function (i) {
+  inv2 <- inv[[i]]
+  lines(inv2$TIME, inv2$PREDICTED_R1, type = lineType, col = col2[i], lwd = lwd, lty = i + 1)
+})
+legend("topright", legend = fileSuffix, title = expression(alpha), lty = seq_along(fileSuffix), col = col2, horiz = F, cex = 0.8)
+
+plot(df$TIME, df$R2, type = 'l', xlab = xlab, col = col[2], lwd = lwd, xlim = xlim, ylab = expression(Omega),
+     main = substitute(bold(R[s ~ x ~ L ~ mm]), list(s = mmBase * 5, L = mmBase * 3)),
+     ylim = c(min(sapply(inv, function (i) {min(na.omit(i$PREDICTED_R2))}), df$R2), max(sapply(inv, function (i) {max(na.omit(i$PREDICTED_R2))}), df$R2)))
+lapply(seq_along(fileSuffix), function (i) {
+  inv2 <- inv[[i]]
+  lines(inv2$TIME, inv2$PREDICTED_R2, type = lineType, col = col2[i], lwd = lwd, lty = i + 1)
+})
+legend("topright", legend = fileSuffix, title = expression(alpha), lty = seq_along(fileSuffix), col = col2, horiz = F, cex = 0.8)
+
+plot(df$TIME, df$ANGLE, type = 'l', col = col[3], lwd = lwd, xlim = xlim, xlab = xlab, ylab = 'градусы',
+     main = 'Угол разгибания кисти')
+
+# Графики параметров модели
 lapply(seq_along(fileSuffix), function (i) {
   inv2 <- inv[[i]]
   if (i == 1) {
