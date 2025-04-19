71b38d2dae22dad70f28267687aed03738d44b9a
Alexander Kobelev <ak.mail.ru@gmail.com>
2025-04-19 15:28:43 +0300

    2025-04-04 E-9682 ak 7 мм

--- main-1.R
+++ main-1.R
@@ -1,22 +1,13 @@
 library('scales')
 mmBase <- 7
 
-start <- 73.76
-begin <- 73.76
-begin <- begin + 2.42
-begin <- begin + 2.43
-begin <- begin + 2.42
-begin <- begin + 2.42
-begin <- begin + 2.43
-begin <- begin + 2.43
-begin <- begin + 2.42
-begin <- begin + 2.44
-begin <- begin + 2.42
-begin <- begin + 2.42
-begin <- begin + 2.42
-begin <- begin + 2.43
-begin <- begin + 2.43
-interval <- (start * 1000 + 1):((begin + 2) * 1000)
+interval <- (13 * 1000 + 1):(23 * 1000)
+interval <- (34 * 1000 + 1):(44 * 1000)
+interval <- (45 * 1000 + 1):(55 * 1000)
+
+interval <- (75 * 1000 + 1):(84 * 1000)
+interval <- (85 * 1000 + 1):(95 * 1000)
+
 
 source(file = 'read.R')
 
@@ -29,3 +20,5 @@ plot(df$TIME, df$R1, type = 'l', xlab = xlab, col = col[1], lwd = 2,
 plot(df$TIME, df$R2, type = 'l', xlab = xlab, col = col[2], lwd = 2,
      ylab = substitute(bold(R[s ~ x ~ L ~ mm] ~ ~Omega), list(s = mmBase * 5, L = mmBase * 3)))
 plot(df$TIME, df$POSITION, type = 'l', xlab = xlab, col = col[3], lwd = 2, ylab = 'POSITION, mm')
+
+paste(round(mean(df$R1), digits = 3), round(mean(df$R2), digits = 3), sep = ", ")
